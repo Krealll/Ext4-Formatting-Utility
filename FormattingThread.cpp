46192e6a0de4719696555063d59f772adcea5de8
Kirill <45152125+Krealll@users.noreply.github.com>
2020-06-03 23:31:11 +0300

    Update FormattingThread.cpp

--- FormattingThread.cpp
+++ FormattingThread.cpp
@@ -22,77 +22,40 @@ void FormattingThread::run()
 
 bool FormattingThread::formatDevice()
 {
-    if(1/*threadFormatManager.initialiseData()*/)
-    {
-        emit send("-Initialized new file system");
-        sleep(1);
-        if(1/*threadFormatManager.manageTables()*/)
-        {
-            emit send("-Writed inode tables");
-            sleep(1);
-            if(1/*threadFormatManager.createDirectories()*/)
-            {
-                emit send("-Created root and lost+found direcories");
-                sleep(1);
-                if(1/*threadFormatManager.writeReservedInodes()*/)
-                {
-                    emit send("-Writed reserved Inodes");
-                    sleep(1);
-                    if(1/*threadFormatManager.endFormatting()*/){
-                        emit send("-Formatting ended");
-                        return true;
-                    }
-                    else{
-                        infoString="ERROR: while initialising file system";
-                    }
-                }else{
-                    infoString="ERROR: while writing tables";
-                }
-            }else{
-                infoString="ERROR: while creating directories";
-            }
-        }else{
-            infoString="ERROR: while writing reserved inodes";
-        }
-    }else{
-        infoString="ERROR: while closing file system";
-    }
-    return  false;
-
-//    if(threadFormatManager.initialiseData())
-//    {
-//        emit send("-Initialized new file system");
-//        sleep(1);
-//        if(threadFormatManager.manageTables())
-//        {
-//            emit send("-Writed inode tables");
-//            sleep(1);
-//            if(threadFormatManager.createDirectories())
-//            {
-//                emit send("-Created root and lost+found direcories");
-//                sleep(1);
-//                if(threadFormatManager.writeReservedInodes())
-//                {
-//                    emit send("-Writed reserved Inodes");
-//                    sleep(1);
-//                    if(threadFormatManager.endFormatting()){
-//                        emit send("-Formatting ended");
-//                        return true;
-//                    }
-//                    else{
-//                        infoString="ERROR: while initialising file system";
-//                    }
-//                }else{
-//                    infoString="ERROR: while writing tables";
-//                }
-//            }else{
-//                infoString="ERROR: while creating directories";
-//            }
-//        }else{
-//            infoString="ERROR: while writing reserved inodes";
-//        }
-//    }else{
-//        infoString="ERROR: while closing file system";
-//    }
-//    return false;
+   if(threadFormatManager.initialiseData())
+   {
+       emit send("-Initialized new file system");
+       sleep(1);
+       if(threadFormatManager.manageTables())
+       {
+           emit send("-Writed inode tables");
+           sleep(1);
+           if(threadFormatManager.createDirectories())
+           {
+               emit send("-Created root and lost+found direcories");
+               sleep(1);
+               if(threadFormatManager.writeReservedInodes())
+               {
+                   emit send("-Writed reserved Inodes");
+                   sleep(1);
+                   if(threadFormatManager.endFormatting()){
+                       emit send("-Formatting ended");
+                       return true;
+                   }
+                   else{
+                       infoString="ERROR: while initialising file system";
+                   }
+               }else{
+                   infoString="ERROR: while writing tables";
+               }
+           }else{
+               infoString="ERROR: while creating directories";
+           }
+       }else{
+           infoString="ERROR: while writing reserved inodes";
+       }
+   }else{
+       infoString="ERROR: while closing file system";
+   }
+   return false;
 }
